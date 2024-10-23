ca60a1dfc4dbc21e4960d0a3675a0b816ac38947
kkmorrisfam <kkmorrisfam@hotmail.com>
2024-10-22 20:18:49 -0600

    start 1st column

--- src/js/Column.mjs
+++ src/js/Column.mjs
@@ -1,5 +1,22 @@
 import { isSameDate } from "./utils.mjs";
 
+
+//probably won't use this, but leave it for structure
 function renderColumnTemplate() {
-    return ``
+    return `<div class="container column day">
+            <!-- <div class="day"></div> -->
+                <div class="button selectedDay">selected date goes here</div>
+                <ul class="time-group">
+                    <li class="button">filtered time slot</li>
+                    <li class="button">filtered time slot</li>
+                    <li class="button">filtered time slot</li>
+                </ul>
+                </div>
+            </div>
+        </div>`
 }
+
+//get the selected date from calendar, pass in parameter, so it can be done elsewhere
+//compare selected date with api date, create array with matching values
+//create new array with filtered value of time slot, for each time an event occurs
+//dynamically create html for column with selected date and time slots into lists
