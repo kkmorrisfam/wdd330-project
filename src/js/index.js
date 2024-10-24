85a3944a00fff741c402fa61b99adb1004334cfd
kkmorrisfam <kkmorrisfam@hotmail.com>
2024-10-24 15:39:06 -0600

    dark mode

--- src/js/index.js
+++ src/js/index.js
@@ -47,6 +47,22 @@ document.addEventListener('DOMContentLoaded', ()=> {
     });
 });  //end of eventListener on initial load
 
+const darkModeToggle = document.querySelector('#dark-toggle');
+//check for saved preferences, or use light 
+const currentTheme = getLocalStorage('theme') || 'light';
+if (currentTheme==='dark') {
+    document.body.classList.add('dark-mode');
+}
+
+darkModeToggle.addEventListener('click', ()=> {
+    document.body.classList.toggle('dark-mode');
+
+    //save preferences in local storage
+    const theme = document.body.classList.contains('dark-mode')?'dark':'light';
+    setLocalStorage('theme', theme);
+});
+
+
 
 //get data test
 // const myAPIData = new ExternalServices(baseURL, binId, apiKey);
